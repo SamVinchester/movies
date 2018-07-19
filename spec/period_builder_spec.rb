060956cb6a3532f7acf1ee6844dfbf19a76a5fda
patrick <maharevvk@gmail.com>
2018-07-19 21:46:12 +0300

    edit specs for #PeriodBuilder

--- spec/period_builder_spec.rb
+++ spec/period_builder_spec.rb
@@ -11,37 +11,69 @@ require '../cashbox.rb'
 describe Theatre::PeriodBuilder do
   let(:period) { Theatre::PeriodBuilder.new }
 
-  describe '#methods' do
-    context 'when use #description' do
+  describe '#description' do
+    context 'when given true args' do
       subject{period.description('Вечерний сеанс')}
       it{ expect(subject).to include(:description => 'Вечерний сеанс')}
     end
+  end
 
-    context 'when use #filters' do
+  describe '#filters' do
+    context 'when given true args' do
       subject{period.filters('Comedy')}
       it{ expect(subject).to include(:filters => 'Comedy')}
     end
+  end
 
-    context 'when use #price' do
+  describe '#price' do
+    context 'when given true args' do
       subject{period.price(20)}
       it{ expect(subject).to include(:price => 20)}
     end
+  end
 
-    context 'when use #hall' do
+  describe '#hall' do
+    context 'when given true args' do
       subject{period.hall(:red, :blue)}
       it{ expect(subject).to include(:hall => [:red, :blue])}
     end
+  end
 
-    context 'when use #tittle' do
+  describe '#tittle' do
+    context 'when given true args' do
       subject{period.tittle('The Terminator')}
       it{ expect(subject).to include(:tittle => 'The Terminator')}
     end
+  end
 
-    context 'when use #to_hash' do
+  describe '#.to_hash' do
+    context 'when save true results' do
+      before {period.price(10)}
       before {period.tittle('The Terminator')}
+      before {period.hall(:green)}
       subject{period.to_hash('09:00'..'10:00')}
-      it{ expect(subject).to include('09:00'..'10:00' => {:tittle => 'The Terminator'})}
+      it{ expect(subject).to include('09:00'..'10:00' => {:price=>10, :tittle => 'The Terminator', :hall=>[:green]})}
     end
 
+    context 'when price is not given' do
+      before {period.tittle('The Terminator')}
+      before {period.hall(:green)}
+      subject{period.to_hash('09:00'..'10:00')}
+      it{ expect{subject}.to raise_error('price is not specified') }
+    end
+
+    context 'when filters or tittle is not given' do
+      before {period.price(10)}
+      before {period.hall(:green)}
+      subject{period.to_hash('09:00'..'10:00')}
+      it{ expect{subject}.to raise_error('filters is not specified') }
+    end
+
+    context 'when halls is not given' do
+      before {period.price(10)}
+      before {period.tittle('The Terminator')}
+      subject{period.to_hash('09:00'..'10:00')}
+      it{ expect{subject}.to raise_error('hall is not specified') }
+    end
   end
 end
